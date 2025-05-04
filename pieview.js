be5c126c34a92c9cec3f18724a13d1d8dde8e318
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2025-05-04 17:05:55 -0400

    Refined structure and wiped out placeholder graphics.

--- pieview.js
+++ pieview.js
@@ -1,26 +1,7 @@
 
 class PieView {
-    constructor(con, width, height) {
+    constructor(data, con, width, height) 
+    {
         this.con = con;
-        const svg = con.root.append('div')
-            .style('width', `${width}px`)
-            .style('height', `${height}px`)
-        .append('svg')
-            .attr('width', width)
-            .attr('height', height)
-        .append('g')
-            .attr('transform', `translate(${width / 2}, ${height / 2})`);
-
-        const data = [10, 20, 30, 40];
-        const pie = d3.pie()(data);
-        const arc = d3.arc().innerRadius(0).outerRadius(Math.min(width, height) / 2 - 10);
-
-        svg.selectAll("path")
-            .data(pie)
-            .enter()
-            .append("path")
-            .attr("d", arc)
-            .attr("fill", (d, i) => con.color(i))
-            .attr("stroke", "black");
     }
 }
--- scatterview.js
+++ scatterview.js
@@ -1,8 +1,38 @@
 class ScatterView
 {
-    constructor (con, width, hight)
+    constructor (data, con, width, height)
     {
-        this.con = con;
-        
+        // Create SVG container
+        const svg = con.root.append('div')
+            .style('width', `${width}px`)
+            .style('height', `${height}px`)
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .append('g')
+            .attr('transform', 'translate(50, 30)');
+
+        const plotWidth = width - 70;
+        const plotHeight = height - 60;
+
+        // Example scales
+        const xScale = d3.scaleLinear()
+            .domain([0, 10])  // Placeholder
+            .range([0, plotWidth]);
+
+        const yScale = d3.scaleLinear()
+            .domain([0, 10])  // Placeholder
+            .range([plotHeight, 0]);
+
+        // Add X Axis
+        svg.append('g')
+            .attr('transform', `translate(0, ${plotHeight})`)
+            .call(d3.axisBottom(xScale));
+
+        // Add Y Axis
+        svg.append('g')
+            .call(d3.axisLeft(yScale));
+
+        console.log(data);
     }
 }
